d76be47afa0148d897181fcc8bb7bf21d865303f
R-Morton <remann12@gmail.com>
2023-07-14 09:32:31 +1000

    basic user model added

---
+++ src/models/UserModels.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+// Create User Schema
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }, 
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    // Foreign object of lists that belong to users
+    lists: [{ type: mongoose.Types.ObjectId, ref: 'List' }]
+})
+
+// Attach schema to model
+const User = mongoose.model('User', UserSchema)
+
+module.exports = {User}
