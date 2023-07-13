1d515607175ac783eb61965299c1cdfab47bbee0
R-Morton <remann12@gmail.com>
2023-07-13 13:34:54 +1000

    database setup complete

--- src/index.js
+++ src/index.js
@@ -3,9 +3,5 @@ var {app, PORT, HOST} = require('./server');
 
 // Run the server
 app.listen(PORT, HOST, () => {
-    console.log(`
-    ExpressJS Blog API is now running!
-
-    Congrats!
-    `);
+    console.log(`Minima-List Server Running!`);
 });
