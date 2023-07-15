d02c2fe51c72faad76d26a928c28e0f8a95ccf19
mastra90 <lukejmastrocola@gmail.com>
2023-07-15 13:49:33 +1000

    Previous commit did not take. Added delete_all method

--- src/controllers/groups_controller.js
+++ src/controllers/groups_controller.js
@@ -1,20 +1,26 @@
 const Group = require('../models/groups_model')
 
-const getGroups = (request, response) => {
-    response.json(
-        {"message": "groups go here"}
-    )
+const getGroups = async (request, response) => {
+    
+    let groups = await Group.find()
+    response.send(groups)
 }
 
-const createGroup = (request, response) => {
+const createGroup = async (request, response) => {
     let newGroup = new Group({
         group_name: request.body.group_name,
         group_members: request.body.group_members
     })
-    newGroup.save()
+    await newGroup.save()
+    response.status(201)
+    response.send(newGroup)
+}
+
+const deleteAllGroups = async (request, response) => {
+    await Group.deleteMany()
     response.json({
-        group: newGroup
+        "message": "All Groups deleted."
     })
 }
 
-module.exports = {getGroups, createGroup}
+module.exports = {getGroups, createGroup, deleteAllGroups}
--- src/routes/groups_routes.js
+++ src/routes/groups_routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const groupsRouter = express.Router();
-const {getGroups, createGroup} = require('../controllers/groups_controller')
+const {getGroups, createGroup, deleteAllGroups} = require('../controllers/groups_controller')
 
 groupsRouter.get("/",getGroups)
 
 groupsRouter.post("/",createGroup)
+
+groupsRouter.delete ("/delete_all",deleteAllGroups)
 
 module.exports = groupsRouter
