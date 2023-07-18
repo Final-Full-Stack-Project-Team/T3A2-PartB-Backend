b54df984836bfb2d6b378d732fd89e836e78140b
mastra90 <lukejmastrocola@gmail.com>
2023-07-18 10:13:01 +1000

    made the groups and users routes page layout the same

--- src/routes/groups_routes.js
+++ src/routes/groups_routes.js
@@ -3,19 +3,11 @@ const groupsRouter = express.Router();
 const {getGroups, getGroup, createGroup, updateGroup, deleteAllGroups, deleteGroup} = require('../controllers/groups_controller')
 
 // Define routes for handling group-related operations
-// GET route to retrieve all groups
 groupsRouter.get("/", getGroups)
-
 groupsRouter.get("/:id", getGroup)
-
-// POST route to create a new group
 groupsRouter.post("/", createGroup)
-
 groupsRouter.put("/:id", updateGroup)
-
-// DELETE route to delete all groups
 groupsRouter.delete ("/delete_all", deleteAllGroups)
-
 groupsRouter.delete ("/:id", deleteGroup)
 
 // Export the groupsRouter to be used in other modules
