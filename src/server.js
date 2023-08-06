f9fcb65d4c410d6c85d36829070dcfcf26a35469
R-Morton <remann12@gmail.com>
2023-08-06 12:49:36 +1000

    fixed cors again

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ app.use(helmet.contentSecurityPolicy({
 // array of origins for CORS configuration.
 const cors = require('cors');
 var corsOptions = {
-    origin: ["http://localhost:3000", "https://sage-churros-00fcd2.netlify.app/sign-in"],
+    origin: ["http://localhost:3000", "https://sage-churros-00fcd2.netlify.app"],
     optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions));
